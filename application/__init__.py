6eaa31ba8ceea0e4d2e3a67f3159dd6b7a6dca98
Deivid <deivid.araujo@multilaser.com.br>
2020-09-08 00:51:34 -0300

    Adicionado suporte para migracoes

--- application/__init__.py
+++ application/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
+from flask_migrate import Migrate
 
 # Bibliotecas acessíveis globalmente
 db = SQLAlchemy()
 login_manager = LoginManager()
+migrate = Migrate()
 
 def create_app():
     """Inicia a aplicação principal"""
@@ -15,11 +16,11 @@ def create_app():
     # Inicializa os plugins
     db.init_app(app)
     login_manager.init_app(app)
+    migrate.init_app(app, db)
 
     with app.app_context():
         # Inclui as rotas
         from . import routes, models
-        db.create_all()
 
         # Registra as  Blueprints
         # app.register_blueprint(auth.auth_bp)
