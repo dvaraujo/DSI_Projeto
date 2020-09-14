36a445d510574a3a3370fd5e9e31d0d22b7ea0a3
dvaraujo <deivid.araujo@aluno.faculdadeimpacta.com.br>
2020-09-14 00:03:43 -0300

    CRUD de usuarios implementado

--- application/routes.py
+++ application/routes.py
@@ -16,8 +16,8 @@ def load_user(user_id):
 
 @login_manager.unauthorized_handler
 def unauthorized():
-    flash('Faça login para acessar esta página 😶', category='error')
-    return redirect('login')
+    flash("Faça login para acessar esta página 😶", category="error")
+    return redirect("login")
 
 
 @app.route("/home")
@@ -26,17 +26,15 @@ def home():
 
     usuarios = Usuario.query.all()
 
-    return render_template('home.html', usuarios=usuarios)
+    return render_template("home.html", usuarios=usuarios)
 
-
-@app.route("/login", methods=['POST', 'GET'])
+@app.route("/")
+@app.route("/login", methods=["POST", "GET"])
 def login():
-
-    sucesso = False
 
     # Se for POST
     if current_user.is_authenticated:
-        return redirect('home')
+        return redirect("home")
 
     form = LoginForm()
 
@@ -44,15 +42,15 @@ def login():
         usuario = Usuario.query.filter_by(email=form.email.data).first()
         if usuario and usuario.verifica_senha(senha=form.senha.data):
             login_user(usuario)
-            next_page = request.args.get('next')
-            return redirect(next_page or 'home')
-        flash('Combinação de usuário e senha incorreta ☹', category='error')
+            next_page = request.args.get("next")
+            return redirect(next_page or "home")
+        flash("Combinação de usuário e senha incorreta ☹", category="error")
 
     # Se for GET
-    return render_template('autenticacao/login.html', form=form )
+    return render_template("autenticacao/login.html", form=form)
 
 
-@app.route("/registro", methods=['POST', 'GET'])
+@app.route("/registro", methods=["POST", "GET"])
 def registro():
 
     form = CadastroForm()
@@ -70,16 +68,54 @@ def registro():
             usuario.set_senha(form.senha.data)
             db.session.add(usuario)
             db.session.commit()
-            flash('Tudo pronto! Agora você já pode fazer o login 😃', category='message')
-            return redirect('login')
-        flash('Usuário ou email já existe ☹', category='error')
+            flash("Tudo pronto! Agora você já pode fazer o login 😃", category="message")
+            return redirect("login")
+        flash("Usuário ou email já existe ☹", category="error")
 
     # Se for GET
-    return render_template('autenticacao/registro.html', form=form)
+    return render_template("autenticacao/registro.html", form=form)
 
 
-@app.route('/logout', methods=['GET'])
+@app.route("/logout", methods=["GET"])
 @login_required
 def logout():
     logout_user()
-    return redirect('/login')
+    return redirect("/login")
+
+
+@app.route("/usuario/<int:usuario_id>", methods=["GET", "POST"])
+@login_required
+def usuario(usuario_id):
+    usuario = Usuario.query.get_or_404(usuario_id)
+    return render_template("usuario.html", usuario=usuario)
+
+
+@app.route("/usuario/<int:usuario_id>/editar", methods=["GET", "POST"])
+@login_required
+def usuario_editar(usuario_id):
+
+    usuario = Usuario.query.get_or_404(usuario_id)
+
+    # Se gor POST
+    form = CadastroForm()
+
+    if form.validate_on_submit():
+        usuario.usuario = form.usuario.data
+        usuario.email = form.email.data
+        usuario.set_senha(form.senha.data)
+        db.session.commit()
+        flash("Usuário alterado", category="message")
+        return render_template("usuario.html", usuario=usuario)
+
+    # Se for GET
+    return render_template("usuario_editar.html", usuario=usuario, form=form)
+
+
+@app.route("/usuario/<int:usuario_id>/excluir", methods=["POST"])
+@login_required
+def usuario_excluir(usuario_id):
+    usuario = Usuario.query.get_or_404(usuario_id)
+    db.session.delete(usuario)
+    db.session.commit()
+    flash("Usuário excluído", category="message")
+    return redirect("/home")
