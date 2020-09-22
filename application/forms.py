a5443b670a673a512cc370c14b2435a05863bf3d
ViniciusOML <limavinicius1@live.com>
2020-09-22 18:26:40 -0300

    atualizado

--- application/forms.py
+++ application/forms.py
@@ -11,16 +11,6 @@ class PessoaForm(FlaskForm):
     confirmar_senha = PasswordField('Confirmação de senha', validators=[input_required('Campo obrigatório'), EqualTo(fieldname='senha', message="As senhas não coincidem")])
     registrar = SubmitField('Registrar')
 
-class MultiCheckboxField(SelectMultipleField):
-    widget = widgets.ListWidget(prefix_label=False)
-    option_widget = widgets.CheckboxInput()
-
-class CheckboxForm(FlaskForm):
-    string_of_areas = ['Penal\n Tributário\n Civíl\n Trabalhista\n Consumidor\n Empresarial']
-    list_of_areas = string_of_areas[0].split()
-    # create a list of value/description tuples
-    area = [(x, x) for x in list_of_areas]
-    areas = MultiCheckboxField('Área', choices=area)
 
 class EscritorioForm(FlaskForm):
     razao_social = StringField('Razão Social', validators=[input_required(message="Campo obrigatório")])
@@ -29,7 +19,7 @@ class EscritorioForm(FlaskForm):
     email = StringField('Email', validators=[Email(message="Insira um e-mail válido")])
     senha = PasswordField('Senha', validators=[input_required("Campo obrigatório"), Length(min=8, message="A senha deve conter pelo menos 8 caracteres")])
     confirmar_senha = PasswordField('Confirmação de senha', validators=[input_required('Campo obrigatório'), EqualTo(fieldname='senha', message="As senhas não coincidem")])
-    #area =
+    #area = 
     registrar = SubmitField('Registrar')
 
 
