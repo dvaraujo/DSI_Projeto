8c3ea7b259ade431b829f4b86a4d4f14a3b4ee5e
ViniciusOML <limavinicius1@live.com>
2020-09-22 22:01:18 -0300

    wp

--- application/forms.py
+++ application/forms.py
@@ -14,13 +14,12 @@ class PessoaForm(FlaskForm):
 
 class EscritorioForm(FlaskForm):
     razao_social = StringField('Razão Social', validators=[input_required(message="Campo obrigatório")])
-    cnpj = StringField('CNPJ', validators=[input_required(message="Informe apenas os números do CNPJ"), Length(14, message = "CNPJ inválido, informe apenas os números")])
-    n_oab = StringField('Nº OAB', validators=[input_required(message="Informe nº OAB no formato UF123456"), Length(8, message = "Número inválido, use o formato formato UF123456")])     
+    cnpj = StringField('CNPJ', validators=[input_required(message="Informe apenas os números do CNPJ"), Length(min = 14, max = 14, message = "CNPJ inválido, informe apenas os números")])
+    n_oab = StringField('Nº OAB', validators=[input_required(message="Informe nº OAB no formato UF123456"), Length(min = 8, max = 8, message = "Número inválido, use o formato formato UF123456")])     
     email = StringField('Email', validators=[Email(message="Insira um e-mail válido")])
     senha = PasswordField('Senha', validators=[input_required("Campo obrigatório"), Length(min=8, message="A senha deve conter pelo menos 8 caracteres")])
     confirmar_senha = PasswordField('Confirmação de senha', validators=[input_required('Campo obrigatório'), EqualTo(fieldname='senha', message="As senhas não coincidem")])
-    #area = 
-    registrar = SubmitField('Registrar')
+    registrar_escritorio = SubmitField('Registrar Escritório')
 
 
 class LoginForm(FlaskForm):
